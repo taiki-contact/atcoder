d8e80856f29d9f28486930c73fc6671295f37e61
taiki-contact <kgyk1015s67@gmail.com>
2019-06-09 20:57:31 +0900

    ABC128

---
+++ abc128/a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main(){
+	int A,P;
+	cin >> A >> P;
+
+	cout << (A*3 + P)/2 << endl;
+
+	return 0;
+}
